248dd47b6a5fb33732b88288423cbd1cd94cd004
DukHyung Kim <idealismboy@gmail.com>
2024-01-23 23:31:25 +0900

    use converted html

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/flosch/pongo2/v6 v6.0.0 // indirect
--- install/install.go
+++ install/install.go
@@ -1,6 +1,7 @@
 package install
 
 import (
+	"github.com/flosch/pongo2/v6"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 )
@@ -11,6 +12,19 @@ func DefaultRouter(r chi.Router) {
 
 func indexHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("install index"))
+		tpl, err := pongo2.FromFile("install/templates/main.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := pongo2.Context{}
+
+		err = tpl.ExecuteWriter(data, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		//_, _ = w.Write([]byte("install index"))
 	}
 }
